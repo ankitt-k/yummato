7ff2176b342fed2e340f51140319b6d25a6e092a
ANKIT KUMAR <ankitk11964@gmail.com>
2025-08-03 18:56:09 +0530

    Update Cart.jsx

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -20,7 +20,8 @@ const Cart = () => {
           if (cartItems[item._id]>0) {
             return (<div key={index}>
               <div className="cart-items-title cart-items-item">
-              <p>=>></p>
+           <img  src={item.image } alt=""/>
+                
                 
                 <p>{item.name}</p>
                 <p>{currency}{item.price}</p>
